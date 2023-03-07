2de7f9c11660dd440e1aacff34c2d2423c932e7a
Jose Carlos Rodriguez <jcrodriguez@PR547.localdomain>
2023-03-07 10:36:04 +0100

    Finally app

--- Fruits/Fruits/App/ContentView.swift
+++ Fruits/Fruits/App/ContentView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     // MARK: - PROPERTIES
+    @State private var isShowingSettings: Bool = false
+
+    
     var fruits: [Fruit] = fruitsData
     
     // MARK: - BODY
@@ -23,7 +26,19 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Fruits")
+            .navigationBarItems(
+                trailing:
+                    Button(action: {
+                        isShowingSettings = true
+                    }, label: {
+                        Image(systemName: "slider.horizontal.3")
+                    })
+                    .sheet(isPresented: $isShowingSettings, content: {
+                        SettingsView()
+                    })
+            )
         } //: NAVIGATION
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
---
+++ Fruits/Fruits/App/SettingsView.swift
@@ -0,0 +1,103 @@
+//
+//  SettingsView.swift
+//  Fruits
+//
+//  Created by Jose Carlos Rodriguez on 7/3/23.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
+    
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(spacing: 20) {
+                    
+                    // MARK: - SECTION 1
+                    GroupBox(
+                        label:
+                            SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")
+                    ) {
+                        Divider().padding(.vertical, 4)
+                        HStack(alignment: .center, spacing: 10) {
+                            Image("logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 80)
+                                .cornerRadius(9)
+                            Text("Most fruits are naturally low in fat, sodium, and calories. None have choresterol. Fruits are sources of many essential nutrients, including potassium, dietary fiber, vitamins, and much more.")
+                                .font(.footnote)
+                        }
+                    }
+                    
+                    // MARK: - SECTION 2
+                    GroupBox(
+                        label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")
+                    ) {
+                        Divider().padding(.vertical, 4)
+                        
+                        Text("If you wish, you can restart the application by toogle the switch in this box. That way it starts the onboarding process and you will see the welcome screen again.")
+                            .padding(.vertical, 8)
+                            .frame(minHeight: 60)
+                            .layoutPriority(1)
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+                        
+                        Toggle(isOn: $isOnboarding) {
+                            if isOnboarding {
+                                Text("Restarted".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.green)
+                            } else {
+                                Text("Restart".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                        .padding()
+                        .background(
+                            Color(UIColor.tertiarySystemBackground)
+                                .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                        )
+                    }
+                    
+                    // MARK: - SECTION 3
+                    GroupBox(
+                        label: SettingsLabelView(labelText: "Aplication", labelImage: "apps.iphone")
+                    ) {
+                        SettingsRowView(name: "Developer", content: "Jose")
+                        SettingsRowView(name: "Designer", content: "Jose")
+                        SettingsRowView(name: "Compatibility", content: "iOS 14")
+                        SettingsRowView(name: "Website", linkLabel: "jcrsdev", linkDestination: "linktr.ee/jcrsdev")
+                        SettingsRowView(name: "Github", linkLabel: "josecrsdc", linkDestination: "github.com/josecrsdc")
+                        SettingsRowView(name: "SwiftUI", content: "2.0")
+                        SettingsRowView(name: "Version", content: "1.1.0")
+                        
+                    }
+                } //: VSTACK
+                .navigationBarTitle(Text("Settings"), displayMode: .large)
+                .navigationBarItems(
+                    trailing:
+                        Button(action: {
+                            presentationMode.wrappedValue.dismiss()
+                        }, label: {
+                            Image(systemName: "xmark")
+                        })
+                )
+                .padding()
+            } //: SCROLL
+        } //: NAVIGATION
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+            .preferredColorScheme(.dark)
+            .previewDevice("iPhone 11 Pro")
+    }
+}
